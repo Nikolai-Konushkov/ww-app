0b8892acf8c87c7e14f098599e74d0ad3a45083c
Конюшков Николай Константинович <konyushkov.nk@esphere.ru>
2021-07-09 17:29:43 +0300

    Splash screen, geo switch on

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {WebView} from 'react-native-webview';
 export default function App() {
   return (
       <>
-        <WebView source={{uri: 'https://wildwood-co.netlify.app'}} />
+        <WebView source={{uri: 'https://wildwood-co.netlify.app'}} geolocationEnabled={true} />
       </>
   );
 }
